c26d6b4ced87eabae5d234a6ba6cf44f4655ee35
kazuho <kazuho@23330352-1942-0410-abcd-d9b8ebed9e9b>
2008-01-01 01:55:05 +0000

    update the year of copyright disclaimer
    git-svn-id: http://kazuho.31tools.com/svn/q4m/trunk@34 23330352-1942-0410-abcd-d9b8ebed9e9b

--- src/ha_queue.cc
+++ src/ha_queue.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 Cybozu Labs, Inc.
+ * Copyright (C) 2007,2008 Cybozu Labs, Inc.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
