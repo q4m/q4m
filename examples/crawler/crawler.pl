383f6a310f6ba27c8c6872da19427a5d2f61faa8
kazuho <kazuho@23330352-1942-0410-abcd-d9b8ebed9e9b>
2008-04-07 07:33:52 +0000

    replace with an impl. using Parallel::Prefork
    git-svn-id: http://kazuho.31tools.com/svn/q4m/trunk@124 23330352-1942-0410-abcd-d9b8ebed9e9b

--- examples/crawler/crawler.pl
+++ examples/crawler/crawler.pl
@@ -7,43 +7,25 @@ use BSD::Resource;
 use DBI;
 use Getopt::Long;
 use LWP::UserAgent;
-use POSIX qw(:sys_wait_h);
+use Parallel::Prefork;
 
+# setup
 my $dbi = 'dbi:mysql:test;user=root;mysql_enable_utf8=1';
-my $workers = 32;
 my $reqs_per_child = 100;
+my $retry_interval = 1;
 my $max_retries = 5;
-my $retry_interval = 15; # in minutes
-my $help;
+my $num_workers = 10;
+my $benchmark_mode = undef;
+my $ua = LWP::UserAgent->new;
+my $_dbh;
 
 GetOptions(
-    'dbi=s'            => \$dbi,
-    'workers=i'        => \$workers,
-    'reqs-per-child=i' => \$reqs_per_child,
-    'max-reties=i'     => \$max_retries,
-    'retry-interval=i' => \$retry_interval,
-    'help'             => \$help,
-) or exit 99;
-if ($help) {
-    print <<"EOT"
-Usage: $0 [options]
-Options: --dbi=dbi_uri          DBI-style URI of MySQL database
-         --workers=num_workers  number of workers to spawn (or 0 not to spawn)
-         --reqs-per-child=num   number of requests to be handled by a worker
-                                process before it shuts down
-         --max-retries=num      maximum number of retries to fetch a URL upon
-                                failures
-         --retry-interval=min   interval between retries in minutes
-EOT
-;
-    exit 0;
-}
-
-$reqs_per_child = 0 unless $workers;
-
-my $ua = LWP::UserAgent->new;
-
-my $_dbh;
+    'reqs-per-child:i' => \$reqs_per_child,
+    'retry-interval:i' => \$retry_interval,
+    'max-retries:i'    => \$max_retries,
+    'num-workers:i'    => \$num_workers,
+    'benchmark'        => \$benchmark_mode,
+);
 
 sub dbh () {
     return $_dbh if $_dbh;
@@ -57,59 +39,50 @@ sub dbh_disconnect {
     $_dbh = undef;
 }
 
-# load given url and store the result
-sub load_url {
-    my ($id, $url) = @_;
-    # request url, return if failed
-    my $r = $ua->get($url);
-    print $r->code, ' ', $url, "\n";
-    return if $r->code >= 500;
-    # save title
-    dbh->do(
-        'update url set title=? where id=?',
-        {},
-        $r->title,
-        $id,
-    ) or die dbh->errsr;
-    # return
-    1;
-}
+# test if we can connect to db (as well as preloading before fork)
+dbh();
+dbh_disconnect();
 
-# main routine of worker processes
+my $pm = Parallel::Prefork->new({
+    max_workers  => $num_workers,
+    trap_signals => {
+        TERM => 'TERM',
+        USR1 => undef,
+    },
+});
+until ($pm->signal_received) {
+    $pm->start and next;
+    # in child process
+    run();
+    $pm->finish;
+}
+$pm->wait_all_children;
+
+# actual work
 sub run {
     # set priority
     setpriority(PRIO_PROCESS, 0, 10);
-    # main loop
-    my $reqs_handled = 0;
-    while (! $reqs_per_child || $reqs_handled < $reqs_per_child) {
-        # poll for a url
-        dbh->selectall_arrayref(
-            'select queue_wait(?,1)',
-            {},
-            sprintf(
-                'crawler_queue:failed_at+fail_cnt*%d<=%d',
-                $retry_interval,
-                int(time / 60),
-            ),
-        ) or die dbh->errstr;
-        # obtain url (returns 1 row at max., or 0 if timeout)
-        my $rows = dbh->selectall_arrayref(
-            'select url.id,url.url,fail_cnt from url natural join crawler_queue',
-            { Slice => {} },
-        ) or die dbh->errstr;
-        # load url, resubmit the request into queue
-        foreach my $row (@$rows) {
-            if (! load_url($row->{id}, $row->{url})
-                    and $row->{fail_cnt} < $max_retries) {
-                dbh->do(
-                    'insert into crawler_queue (id,fail_cnt,failed_at) values (?,?,?)',
-                    {},
-                    $row->{id},
-                    $row->{fail_cnt} + 1,
-                    int(time / 60),
-                ) or die dbh->errstr;
-            }
-            $reqs_handled++;
+    # loop while we have uris in queue
+    for (my $c = 0; $c < $reqs_per_child; $c++) {
+        # load a url to fetch
+        my ($id, $url, $fail_cnt) = load_url();
+        # fetch url
+        my $response = $ua->get($url);
+        printf "%-5d %s %d %s\n", $$, scalar localtime, $response->code, $url;
+        if ($response->is_success) {
+            # success, store the result
+            dbh->do(
+                'update url set title=? where id=?',
+                {},
+                $response->title || undef, $id,
+            ) or die dbh->errstr;
+        } elsif ($fail_cnt < $max_retries) {
+            # schedule for retry
+            dbh->do(
+                'insert into crawler_queue (id,fail_cnt,failed_at) values (?,?,?)',
+                {},
+                $id, $fail_cnt + 1, int(time / 60),
+            ) or die dbh->errstr;
         }
     }
     dbh->selectrow_array("select queue_end()")
@@ -117,23 +90,26 @@ sub run {
     0;
 }
 
-exit run()
-    if $workers == 0;
-
-# fork workers
-my %pids;
-while (1) {
-    dbh_disconnect;
-    while (scalar(keys %pids) < $workers) {
-        my $pid = fork;
-        die "fork failed:" unless defined $pid;
-        exit run()
-            unless $pid;
-        $pids{$pid} = 1;
-    }
-    my $exit_pid = wait;
-    while ($exit_pid > 0) {
-        delete $pids{$exit_pid};
-        $exit_pid = waitpid(-1, WNOHANG);
+# loads a url to fetch
+sub load_url {
+    while (1) {
+        my @w = dbh->selectrow_array(sprintf(
+            "select queue_wait('crawler_queue:failed_at+%d*pow(2,fail_cnt)-1<=%d',%d)",
+            $retry_interval,
+            int(time / 60),
+            $benchmark_mode ? 0 : 60,
+        )) or die dbh->errstr;
+        unless ($w[0]) {
+            if ($benchmark_mode) {
+                kill 'USR1', $pm->manager_pid;
+                exit 0;
+            }
+            next;
+        }
+        my $rows = dbh->selectall_arrayref(
+            'select url.id,url.url,fail_cnt from url natural join crawler_queue',
+        ) or die dbh->errstr;
+        next unless @$rows;
+        return @{$rows->[0]};
     }
 }
