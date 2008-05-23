20732a7d4ceb8b274683be970281ce7867efeb61
kazuho <kazuho@23330352-1942-0410-abcd-d9b8ebed9e9b>
2008-05-23 11:19:26 +0000

    add multi rw test
    git-svn-id: http://kazuho.31tools.com/svn/q4m/trunk@154 23330352-1942-0410-abcd-d9b8ebed9e9b

---
+++ t/05-multirw-core.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mysql.h>
+
+static MYSQL mysql;
+
+static MYSQL_RES *do_select(const char *sql)
+{
+  MYSQL_RES *res;
+  
+  if (mysql_query(&mysql, sql) != 0) {
+    fprintf(stderr, "SQL error: %s\n", sql);
+    exit(3);
+  }
+  if ((res = mysql_store_result(&mysql)) == NULL) {
+    fprintf(stderr, "failed to obtain result from mysql\n");
+    exit(3);
+  }
+  return res;
+}
+
+int main(int argc, char **argv)
+{
+  const char *host = getenv("MYSQL_HOST"),
+    *user = getenv("MYSQL_USER"),
+    *password = getenv("MYSQL_PASSWORD"),
+    *db = getenv("MYSQL_DB"),
+    *port_str = getenv("MYSQL_PORT"),
+    *unix_socket = getenv("MYSQL_SOCKET");
+  unsigned short port;
+  unsigned i, loop, start_value;
+  MYSQL_RES *res;
+  char sql[256];
+  
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <loop_count> <start_value>\n", argv[0]);
+    exit(1);
+  } else if (sscanf(argv[1], "%u", &loop) != 1) {
+    fprintf(stderr, "invalid loop count: %s\n", argv[1]);
+    exit(1);
+  } else if (sscanf(argv[2], "%u", &start_value) != 1) {
+    fprintf(stderr, "invalid start value: %s\n", argv[2]);
+    exit(1);
+  }
+  if (db == NULL)
+    db = "test";
+  if (port_str != NULL
+      && sscanf(port_str, "%hu", &port) != 1) {
+    fprintf(stderr, "invalid port number: %s\n", port_str);
+    exit(1);
+  }
+  
+  mysql_init(&mysql);
+  if (mysql_real_connect(&mysql, host, user, password, db, port, unix_socket, 0)
+      == NULL) {
+    fprintf(stderr, "could not connect to mysql\n");
+    exit(2);
+  }
+  
+  for (i = 0; i < loop; i++) {
+    /* insert one row */
+    sprintf(sql, "INSERT INTO q4m_t VALUES (%d)", i + start_value);
+    if (mysql_query(&mysql, sql) != 0) {
+      fprintf(stderr, "insert failed\n");
+      exit(3);
+    }
+    /* queue_wait */
+    res = do_select("SELECT queue_wait('q4m_t')");
+    if (mysql_num_rows(res) == 0 || strcmp(mysql_fetch_row(res)[0], "0") == 0) {
+      fprintf(stderr, "queue_wait timeout\n");
+      exit(3);
+    }
+    mysql_free_result(res);
+    /* read data and print */
+    res = do_select("SELECT * FROM q4m_t");
+    if (mysql_num_rows(res) == 0) {
+      fprintf(stderr, "unexpected response from Q4M (no owned rows)\n");
+      exit(3);
+    }
+    printf("%s\n", mysql_fetch_row(res)[0]);
+    mysql_free_result(res);
+  }
+  /* queue_end */
+  res = do_select("SELECT queue_end()");
+  mysql_free_result(res);
+  
+  mysql_close(&mysql);
+  return 0;
+}
