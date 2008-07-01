fbe397854da031985f624aaedb9bb4bc72bddb4d
kazuho <kazuho@23330352-1942-0410-abcd-d9b8ebed9e9b>
2008-07-01 04:05:15 +0000

    update example
    git-svn-id: http://kazuho.31tools.com/svn/q4m/trunk@185 23330352-1942-0410-abcd-d9b8ebed9e9b

--- examples/crawler/crawler.pl
+++ examples/crawler/crawler.pl
@@ -5,6 +5,7 @@ use warnings;
 
 use BSD::Resource;
 use DBI;
+use Filter::SQL;
 use Getopt::Long;
 use LWP::UserAgent;
 use Parallel::Prefork;
@@ -12,7 +13,7 @@ use Parallel::Prefork;
 # setup
 my $dbi = 'dbi:mysql:test;user=root;mysql_enable_utf8=1';
 my $reqs_per_child = 100;
-my $retry_interval = 1;
+my $retry_interval = 5;
 my $max_retries = 5;
 my $num_workers = 10;
 my $benchmark_mode = undef;
@@ -28,21 +29,9 @@ GetOptions(
     'benchmark'        => \$benchmark_mode,
 );
 
-sub dbh () {
-    return $_dbh if $_dbh;
-    $_dbh = DBI->connect($dbi)
-         or die DBI->errstr;
-    $_dbh;
-}
-
-sub dbh_disconnect {
-    $_dbh->disconnect if $_dbh;
-    $_dbh = undef;
-}
-
 # test if we can connect to db (as well as preloading before fork)
-dbh();
-dbh_disconnect();
+connect_db();
+Filter::SQL->dbh->disconnect;
 
 my $pm = Parallel::Prefork->new({
     max_workers  => $num_workers,
@@ -51,66 +40,90 @@ my $pm = Parallel::Prefork->new({
         USR1 => undef,
     },
 });
+
+# start first $max_retries processes for rescheduling failed requests
+my @resched_pids;
+for (my $i = 1; $i < $max_retries; $i++) {
+    my $pid = fork;
+    die 'fork error' unless defined $pid;
+    unless ($pid) {
+        # in child process
+        connect_db();
+        run_rescheduler($i);
+        exit 0;
+    }
+    push @resched_pids, $pid;
+}
+
+# main loop
 until ($pm->signal_received) {
     $pm->start and next;
     # in child process
+    connect_db();
     run();
     $pm->finish;
 }
+
+# kill all reschedulers, and wait for children
+foreach my $p (@resched_pids) {
+    kill 'TERM', $p;
+    while ($p != waitpid($p, 0)) {
+    }
+}
 $pm->wait_all_children;
+
 
 # actual work
 sub run {
     # set priority
     setpriority(PRIO_PROCESS, 0, 10);
     # loop while we have uris in queue
-    for (my $c = 0; $c < $reqs_per_child; $c++) {
+    my $loop_cnt = 0;
+    while ($loop_cnt < $reqs_per_child) {
         # load a url to fetch
-        my ($id, $url, $fail_cnt) = load_url();
+        my ($id, $fail_cnt) =
+            SELECT ROW id,fail_cnt FROM crawler_queue
+                WHERE queue_wait('crawler_queue');
+            or next;
+        my ($url) = SELECT ROW url FROM url WHERE id=$id;
+            or next;
         # fetch url
         my $response = $ua->get($url);
         printf "%-5d %s %d %s\n", $$, scalar localtime, $response->code, $url;
         if ($response->is_success) {
             # success, store the result
-            dbh->do(
-                'update url set title=? where id=?',
-                {},
-                $response->title || undef, $id,
-            ) or die dbh->errstr;
+            UPDATE url SET title={$response->title || undef} WHERE id=$id;;
         } elsif ($fail_cnt < $max_retries) {
             # schedule for retry
-            dbh->do(
-                'insert into crawler_queue (id,fail_cnt,failed_at) values (?,?,?)',
-                {},
-                $id, $fail_cnt + 1, int(time / 60),
-            ) or die dbh->errstr;
+            INSERT INTO crawler_reschedule_queue (id,fail_cnt,at)
+                VALUES ($id,{$fail_cnt+1},{time});;
         }
+        $loop_cnt++;
     }
-    dbh->selectrow_array("select queue_end()")
-        or die dbh->errstr;
+    SELECT queue_end();;
     0;
 }
 
-# loads a url to fetch
-sub load_url {
+# move entry in resheduler_queue to queue
+sub run_rescheduler {
+    my ($fail_cnt) = @_;
+    my $interval = $retry_interval * 2 ** $fail_cnt;
+    
     while (1) {
-        my @w = dbh->selectrow_array(sprintf(
-            "select queue_wait('crawler_queue:failed_at+%d*pow(2,fail_cnt)-1<=%d',%d)",
-            $retry_interval,
-            int(time / 60),
-            $benchmark_mode ? 0 : 60,
-        )) or die dbh->errstr;
-        unless ($w[0]) {
-            if ($benchmark_mode) {
-                kill 'USR1', $pm->manager_pid;
-                exit 0;
-            }
-            next;
+        my ($id, $fail_cnt, $at) =
+            SELECT ROW id,fail_cnt,at FROM crawler_reschedule_queue
+                WHERE
+                    queue_wait("crawler_reschedule_queue:fail_cnt=$fail_cnt");
+            or next;
+        while (my $sleep_secs = $at + $interval - time > 0) {
+            sleep $sleep_secs;
         }
-        my $rows = dbh->selectall_arrayref(
-            'select url.id,url.url,fail_cnt from url natural join crawler_queue',
-        ) or die dbh->errstr;
-        next unless @$rows;
-        return @{$rows->[0]};
+        INSERT INTO crawler_queue (id,fail_cnt) VALUES ($id,$fail_cnt);;
     }
+}
+
+# connect to db and set handle in Filter::SQL
+sub connect_db {
+    Filter::SQL->dbh(DBI->connect($dbi))
+            or die DBI->errstr;
 }
--- examples/crawler/initdb.sql
+++ examples/crawler/initdb.sql
@@ -8,9 +8,15 @@ CREATE TABLE url (
 
 CREATE TABLE `crawler_queue` (
   `id` int(10) unsigned NOT NULL,
-  `fail_cnt` int(11) NOT NULL DEFAULT 0,
-  `failed_at` int(11) NOT NULL DEFAULT 0
+  `fail_cnt` int(11) NOT NULL DEFAULT 0
 ) ENGINE=QUEUE DEFAULT CHARSET=utf8;
+
+CREATE TABLE `crawler_reschedule_queue` (
+  `id` int(10) unsigned NOT NULL,
+  `fail_cnt` int(11) NOT NULL DEFAULT 0,
+  `at` int(11) NOT NULL DEFAULT 0
+) ENGINE=QUEUE DEFAULT CHARSET=utf8;
+
 
 DELIMITER |
 
